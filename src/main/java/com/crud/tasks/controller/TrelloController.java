170b9def238782d788fb9eb8457e53c02aab24cd
vitiak <kwaswi@wp.pl>
2017-11-11 19:02:57 +0100

    commit zadanie 18.2

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,39 @@
+package com.crud.tasks.controller;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/v1/trello")
+public class TrelloController {
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        List<TrelloBoardDto> list = trelloBoards.stream()
+                .filter(x -> !x.getId().isEmpty() && !x.getName().isEmpty() && x.getName().contains("Kodilla"))
+                .collect(Collectors.toList());
+
+        list.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTrello")
+    public void getTrello() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+    }
+}
