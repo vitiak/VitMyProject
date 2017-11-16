789e3aa26ef12b3f3f0baa420506f8ab40a87e2d
vitiak <kwaswi@wp.pl>
2017-11-16 18:29:12 +0100

    commit zadanie 18.3 zadanie

--- src/main/java/com/crud/tasks/domain/Badges.java
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -7,5 +7,5 @@ import lombok.Getter;
 @Getter
 public class Badges {
     int votes;
-    AttachmentsByType attachments;
+    AttachmentsByType attachmentsByType;
 }
--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -10,7 +10,7 @@ public class TrelloCardDto {
     private String description;
     private String pos;
     private String listId;
-    private Badges badges;
+/*    private Badges badges;*/
 /*
     private AttachmentsByType attachmentsByType;
     private int votes;
