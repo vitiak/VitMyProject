9a669809b862a5d74a76c0d7ad42e2f5859f2547
vitiak <kwaswi@wp.pl>
2017-11-23 22:40:19 +0100

    commit 19.4e

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,61 +3,53 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
-import com.crud.tasks.trello.client.TrelloClient;
+import com.crud.tasks.service.TrelloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/trello")
 public class TrelloController {
 
     @Autowired
-    private TrelloClient trelloClient;
+    private TrelloService trelloService;
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    @RequestMapping(method = RequestMethod.GET, value = "/getTrelloBoards")
     public List<TrelloBoardDto> getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
+        return trelloService.fetchTrelloBoards();
+/*        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
         List<TrelloBoardDto> list = trelloBoards.stream()
                 .filter(x -> !x.getId().isEmpty() && !x.getName().isEmpty() && x.getName().contains("Kodilla"))
                 .collect(Collectors.toList());
-
         list.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
-
         // GET request
-
         trelloBoards.forEach(trelloBoardDto -> {
-
             System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-
             System.out.println("This board contains lists: ");
-
             trelloBoardDto.getLists().forEach(trelloList ->
                     System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
             //trelloBoardDto.getBadges();
 
         });
-        return trelloBoards;
+        return trelloBoards;*/
     }
 
 
+/*
     @RequestMapping(method = RequestMethod.GET, value = "getTrello")
     public void getTrello() {
 
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+36+28
+        List<TrelloBoardDto> trelloBoards = trelloService.getTrelloBoards();
         trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
     }
+*/
 
-    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
+    @RequestMapping(method = RequestMethod.POST, value = "/createTrelloCard")
     public CreatedTrelloCard createdTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
-        return trelloClient.createdNewCard(trelloCardDto);
+        return trelloService.createdTrelloCard(trelloCardDto);
     }
-
 }
---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,42 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Optional.ofNullable;
+
+@Service
+public class TrelloService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @Autowired
+    private SimpleEmailService emailService;
+
+    private static final String SUBJECT = "Tasks: new Trello card";
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCard createdTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createdNewCard(trelloCardDto);
+/*        emailService.send(new Mail(adminConfig.getAdminMail(),SUBJECT,
+                "New card: " + trelloCardDto.getName() + " has been created on your trello account" ));*/
+        ofNullable(newCard).ifPresent(card->emailService.send(new Mail(adminConfig.getAdminMail(),SUBJECT,
+                "New card: " + trelloCardDto.getName() + " has been created on your trello account")));
+        return newCard;
+    }
+}
