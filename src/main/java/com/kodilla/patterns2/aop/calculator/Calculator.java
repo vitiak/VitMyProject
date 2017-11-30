9eb24ec9209c1a1d6eee0eee7806d688115450fc
vitiak <kwaswi@wp.pl>
2017-11-30 19:59:35 +0100

    wyklad 20.3

---
+++ src/main/java/com/kodilla/patterns2/aop/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.patterns2.aop.calculator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+    public double add(double x, double y) {return x + y;}
+    public double sub(double x, double y) {return x - y;}
+    public double mul(double x, double y) {return x * y;}
+    public double div(double x, double y) {
+        if (y==0) {throw new ArithmeticException("Divide by zero!");}
+        return x / y;
+    }
+}
