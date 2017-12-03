bf992415d6f29dbbc379558cab2cd90fdf4f9626
vitiak <kwaswi@wp.pl>
2017-12-03 21:57:25 +0100

    zadanie 21.3?

---
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -0,0 +1,58 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TaskController.class)
+public class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DbService service;
+
+    @MockBean
+    private TaskMapper taskMapper;
+
+
+    @Test
+    public void shouldFetchListTasks() throws Exception {
+        //Given
+        List<Task> taskLists = new ArrayList<>();
+        taskLists.add(new Task(1, "title1", "content1"));
+        List<TaskDto> listTaskDto = taskMapper.mapToTaskDtoList(taskLists);
+
+        when(taskMapper.mapToTaskDtoList(service.getAllTasks())).thenReturn(listTaskDto);
+
+        //When & Then
+
+        mockMvc.perform(get("/v1/Task/getTasks").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect( jsonPath("$", hasSize(1)))
+                .andExpect( jsonPath("$[0].id", is(1)))
+                .andExpect( jsonPath("$[0].title", is("title1")))
+                .andExpect( jsonPath("$[0].content", is("content1")));
+    }
+}
--- src/test/java/com/crud/tasks/controller/TrelloControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TrelloControllerTest.java
@@ -1,25 +1,29 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCardDto;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.domain.TrelloListDto;
 import com.crud.tasks.trello.facade.TrelloFacade;
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -62,14 +66,41 @@ public class TrelloControllerTest {
         mockMvc.perform(get("/v1/trello/getTrelloBoards").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 //trelloBoard fields
-                .andExpect((ResultMatcher) jsonPath("$", hasSize(1)))
-                .andExpect((ResultMatcher) jsonPath("$[0].id", is("1")))
-                .andExpect((ResultMatcher) jsonPath("$[0].name", is("Test Task")))
+                .andExpect( jsonPath("$", hasSize(1)))
+                .andExpect( jsonPath("$[0].id", is("1")))
+                .andExpect( jsonPath("$[0].name", is("Test Task")))
         //trelloList fields
-                .andExpect((ResultMatcher) jsonPath("$[0].lists", hasSize(1)))
-                .andExpect((ResultMatcher) jsonPath("$[0].lists[0].id", is("1")))
-                .andExpect((ResultMatcher) jsonPath("$[0].lists[0].name", is("Test List")))
-                .andExpect((ResultMatcher) jsonPath("$[0].lists[0].closed", is(false)));
+                .andExpect( jsonPath("$[0].lists", hasSize(1)))
+                .andExpect( jsonPath("$[0].lists[0].id", is("1")))
+                .andExpect( jsonPath("$[0].lists[0].name", is("Test List")))
+                .andExpect( jsonPath("$[0].lists[0].closed", is(false)));
+    }
+
+    @Test
+    public void shouldCreateTrelloCard() throws Exception {
+        //Given
+        TrelloCardDto trelloCardDto = new TrelloCardDto(
+                "Test",
+                "Test description",
+                "top",
+                "1" );
+        CreatedTrelloCardDto createdTrelloCardDto = new CreatedTrelloCardDto(
+                "323",
+                "Test",
+                "http://test.com");
+        when(trelloFacade.createdCard(ArgumentMatchers.any(TrelloCardDto.class))).thenReturn(createdTrelloCardDto);
+
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(trelloCardDto);
+
+        // When & Then
+        mockMvc.perform(post("/v1/trello/createTrelloCard")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent))
+                .andExpect(jsonPath("$.id", is("323")))
+                .andExpect(jsonPath("$.name", is("Test")))
+                .andExpect(jsonPath("$.shortUrl", is("http://test.com")));
 
     }
 
