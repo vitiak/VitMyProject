3159c942f20d9ce85fccaab66a403a37c29f362b
vitiak <kwaswi@wp.pl>
2017-12-09 12:11:28 +0100

    wyklad 21.3

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,22 +3,15 @@ package com.crud.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
-public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication /*extends SpringBootServletInitializer*/ {
 
 	public static void main(String[] args) {
-//		TaskDto taskDto = new TaskDto((long) 1,	"Test title","I want to be a coder"	);
 		SpringApplication.run(TasksApplication.class, args);
-
-//		Long id = taskDto.getId();
-//		String title = taskDto.getTitle();
-//		String content = taskDto.getContent();
-//		System.out.println(id + " " + title + " " + content);
 	}
 
-/*	@Override
+	/*	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TasksApplication.class);
 	}*/
