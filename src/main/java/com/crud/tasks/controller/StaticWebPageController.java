742bab55bf585855f39acfd23fcb3faf612fbf2e
vitiak <kwaswi@wp.pl>
2017-12-12 19:12:10 +0100

    zadanie 24.1

---
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -0,0 +1,22 @@
+package com.crud.tasks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class StaticWebPageController {
+
+    @RequestMapping("/")
+    public String index(Map<String, Object> model) {
+        model.put("variable", "My Thymeleaf variable");
+        model.put("two", 2);
+        model.put("add", "+");
+        model.put("equals", "=");
+        model.put("substr", "-");
+        model.put("mult", "*");
+        return "index";
+    }
+
+}
