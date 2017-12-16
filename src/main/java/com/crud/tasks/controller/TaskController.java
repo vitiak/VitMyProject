452205e37572b55234da41de7312033ee5bd487c
vitiak <kwaswi@wp.pl>
2017-12-16 20:52:54 +0100

    wyklad 24.4

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -13,21 +13,24 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @CrossOrigin(origins = "*")
 @RestController /*wystawienie publiczne*/
-@RequestMapping("/v1/task")/*definiuje adres na jaki jest wystawione*/
+//@RequestMapping("/v1/task")/*definiuje adres na jaki jest wystawione*/
+@RequestMapping("/v1")/*definiuje adres na jaki jest wystawione*/
 public class TaskController {
     @Autowired
     private DbService service;
     @Autowired
     private TaskMapper taskMapper;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getTasks") /**/
+//    @RequestMapping(method = RequestMethod.GET, value = "/getTasks") /**/
+    @RequestMapping(method = RequestMethod.GET, value = "/tasks") /**/
     public List<TaskDto> getTasks() {
         List<Task> listTask = service.getAllTasks();
         List<TaskDto> listTaskDto = taskMapper.mapToTaskDtoList(listTask);
         return listTaskDto;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getTask")
+//    @RequestMapping(method = RequestMethod.GET, value = "/getTask")
+    @RequestMapping(method = RequestMethod.GET, value = "/tasks/{}taskId")
     public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
 //        return new TaskDto((int)1, "test title", "test_content");
         Optional<Task> optionalTask = service.getTask(taskId);
@@ -36,20 +39,21 @@ public class TaskController {
         return taskDto;
     }
 
-
-    @RequestMapping(method = RequestMethod.DELETE, value = "/deleteTask")
+    //@RequestMapping(method = RequestMethod.DELETE, value = "/deleteTask")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/tasks/{taskId}")
     public void deleteTask(@RequestParam Long taskId) throws TaskNotFoundException {
         service.deleteTask(taskId);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/createTask", consumes = APPLICATION_JSON_VALUE)
+    //@RequestMapping(method = RequestMethod.POST, value = "/createTask", consumes = APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.POST, value = "/tasks", consumes = APPLICATION_JSON_VALUE)
     public void createTask(@RequestBody TaskDto taskDto) {
         service.saveTask(taskMapper.mapToTask(taskDto));
     }
 
 
-
-    @RequestMapping(method = RequestMethod.PUT, value = "/updateTask", consumes = APPLICATION_JSON_VALUE)
+//    @RequestMapping(method = RequestMethod.PUT, value = "/updateTask", consumes = APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.PUT, value = "/tasks")
     public void updateTask(@RequestBody TaskDto taskDto) {
         service.saveTask(taskMapper.mapToTask(taskDto));
     }
